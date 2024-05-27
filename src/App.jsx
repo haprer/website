d54886ea5abc339934461ea94f09d6bf7ec1b38e
Harper <harpersullivan00@gmail.com>
2024-05-27 18:04:44 -0400

    made a few changes, removed vite images

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import './index.css';
 
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const darkPal = window.getComputedStyle(document.documentElement).getPropertyValue('--dark-pal').trim();
+  const lightPal = window.getComputedStyle(document.documentElement).getPropertyValue('--grey-pal').trim();
+  const acc1Pal = window.getComputedStyle(document.documentElement).getPropertyValue('--accent1-pal').trim();
+  const acc2Pal = window.getComputedStyle(document.documentElement).getPropertyValue('--accent2-pal').trim();
+  const acc3Pal = window.getComputedStyle(document.documentElement).getPropertyValue('--accent3-pal').trim();
+
 
   return (
-    <>
-      <div>
-        <Parallax pages={4}>
-          <ParallaxLayer
-            offset={0}
-            style={{backgroundColor:'whitesmoke'}}
-            factor={.3}
-            speed={0}
-          >
-            <div>
-              <h1>Harper Sullivan</h1>
-            </div>
-          </ParallaxLayer>
-          <ParallaxLayer offset={.5} speed={1}>
-            <div>
-            <a href="https://vitejs.dev" target="_blank">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
-            <p>
-              Edit <code>src/App.jsx</code> and save to test HMR
-            </p>
-            </div>
-          </ParallaxLayer>
-         
-        </Parallax>
-        
-      </div>
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-    </>
+    <Parallax pages={2} className="parallax">
+      <ParallaxLayer
+        offset={0}
+        factor={.3}
+        speed={0}
+      >
+        <div>
+          <h1>Harper Sullivan</h1>
+        </div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={.5} speed={1} className="parallaxLayer">
+        <div className="container">
+          <h1>Check out my profiles:</h1>
+          <div id="links">
+            <a href="https://github.com/haprer">Harper on Github</a>
+            <br />
+            <a href="https://github.com/clifton4">Clifton on Github (an older account)</a>
+          </div>    
+        </div>  
+      </ParallaxLayer>
+      
+    </Parallax>
   )
 }
 
