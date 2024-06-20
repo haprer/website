6a535d3c332e321923e3fb59df0f4d34d18e6c66
haprer <harpersullivan00@gmail.com>
2024-06-19 22:39:01 -0400

    second accordion tab just for fun

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,12 +19,9 @@ function QuestionAccordian() {
                     Bonus points if you have a cool office, and extra if it&apos;s in Europe. 
                 </p>
             </AccordionTab>
-            <AccordionTab header="Header II">
+            <AccordionTab header="Do you have a degree?">
                 <p className="m-0">
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                    quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
-                    sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-                    Consectetur, adipisci velit, sed quia non numquam eius modi.
+                    Yes, I have a Bachelor of Science in Computer Science from the University of New Hampshire. 
                 </p>
             </AccordionTab>
             <AccordionTab header="Header III">
